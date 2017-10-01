12bdc0eb378a687bcb4fd3d4cc854e70628eefe4
Richard Hill III <rhillz718@gmail.com>
2017-10-01 03:45:09 -0400

    created albumDetail component.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header'
 import AlbumList from './src/components/AlbumList'
+;
 
 export default class App extends React.Component {
   render() {
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
 
 
 export default class AlbumList extends Component{
@@ -19,7 +21,7 @@ export default class AlbumList extends Component{
 
     renderAlbums(){
         return this.state.albums.map(album => 
-        <Text key={album.title}>{album.title}</Text>
+        <AlbumDetail key={album.title} album={album}/>
         
         );
 
