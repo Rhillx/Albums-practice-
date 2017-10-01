5a771a1d3a4347680e7e831c257cc0cf63b4964d
Richard Hill III <rhillz718@gmail.com>
2017-10-01 15:01:06 -0400

    added button to link to amazon and buy the selcted album

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Linking} from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) =>{
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
     const { 
         cardHeaderContent, 
         thumbnailContainer, 
@@ -34,6 +35,11 @@ const AlbumDetail = ({album}) =>{
                 style={imageStyle} 
                 source={{uri: image }}
                 />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
